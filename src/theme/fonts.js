0c21e12f065e18a039d862dd8cab57ab5f48fe5c
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-12 23:21:01 +0900

    [Style] font 피그마 모두 추가

--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -1,38 +1,32 @@
+const createFontStyle = (size, weight, lineHeight, letterSpacing) => `
+    font-size: ${size}px;
+    font-weight: ${weight};
+    line-height: ${lineHeight}%; 
+    letter-spacing: ${letterSpacing}%;
+`
 export const fonts = {
-  header0: `
-    font-size: 44px;
-    font-weight: 800;
-    line-height: 160%; 
-    letter-spacing: -10%;
-  `,
-  header1: `
-    font-size: 44px;
-    font-weight: bold;
-    line-height: 160%;
-    letter-spacing: 0;
-  `,
-  header2: `
-    font-size: 40px;
-    font-weight: bold;
-    line-height: 160%;
-    letter-spacing: 0;
-  `,
-  header3: `
-    font-size: 36px;
-    font-weight: bold;
-    line-height: 160%;
-    letter-spacing: 0;
-  `,
-  body1: `
-    font-size: 16px;
-    font-weight: bold;
-    line-height: 160%;
-    letter-spacing: 0;
-  `,
-  caption1: `
-    font-size: 12px;
-    font-weight: medium;
-    line-height: 160%;
-    letter-spacing: 0;
-  `
+  header0: createFontStyle(44, 'bold', 160, -10),
+  header1: createFontStyle(44, 'bold', 160, 0),
+  header2: createFontStyle(40, 'bold', 160, 0),
+  header3: createFontStyle(36, 'bold', 160, 0),
+  header4: createFontStyle(32, 'bold', 160, 0),
+  header5: createFontStyle(28, 'bold', 160, 0),
+  header6: createFontStyle(24, 'bold', 160, 0),
+
+  sub_header1: createFontStyle(24, 'bold', 160, 0),
+  sub_header2: createFontStyle(24, 600, 160, 0),
+  sub_header3: createFontStyle(20, 'bold', 160, 0),
+  sub_header4: createFontStyle(20, 600, 160, 0),
+  sub_header5: createFontStyle(18, 'bold', 160, 0),
+  sub_header6: createFontStyle(18, 600, 160, 0),
+
+  body1: createFontStyle(20, 'bold', 160, 0),
+  body2: createFontStyle(20, 600, 160, 0),
+  body3: createFontStyle(16, 'bold', 160, 0),
+  body4: createFontStyle(16, 600, 160, 0),
+
+  caption1: createFontStyle(14, 'bold', 160, 0),
+  caption2: createFontStyle(14, 600, 160, 0),
+  caption3: createFontStyle(12, 'bold', 160, 0),
+  caption4: createFontStyle(12, 600, 160, 0),
 };
