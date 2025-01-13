cfe7894cf72736577f886edc9e5a74012e29ede0
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-13 17:06:56 +0900

    [Chore] 나눔고딕 폰트 내장 및 연결

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+// icons
+
+// images
+
+// fonts
+export {default as NanumGothicRegular } from './fonts/NanumGothic-Regular.ttf';
+export {default as NanumGothicBold } from './fonts/NanumGothic-Bold.ttf';
+export {default as NanumGothicExtraBold } from './fonts/NanumGothic-ExtraBold.ttf';
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,8 +1,27 @@
 import { createGlobalStyle } from "styled-components";
+import {NanumGothicRegular, NanumGothicBold, NanumGothicExtraBold} from "../assets";
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: "NanumGothic-Regular";
+        font-weight: 400;
+        src: url(${NanumGothicRegular}) format("truetype");
+    }
+
+    @font-face {
+        font-family: "NanumGothic-Bold";
+        font-weight: 700;
+        src: url(${NanumGothicBold}) format("truetype");
+    }
+
+    @font-face {
+        font-family: "NanumGothic-ExtraBold";
+        font-weight: 800;
+        src: url(${NanumGothicExtraBold}) format("truetype");
+    }
+    
   body {
-    font-family: 'Nanum Gothic', sans-serif;
+    font-family: 'NanumGothic-Regular',sans-serif;
   }
 `;
 
