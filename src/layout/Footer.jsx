91819cb083953793d5d4315277447a19fa30bb26
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-13 17:45:52 +0900

    [Chore] Footer불필요 코드 삭제

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { styled } from "styled-components"
 
 const Footer = () => {
   return (
     <Container>
-      <FooterContainer>푸터영역</FooterContainer>
+      <FooterContainer></FooterContainer>
     </Container>
   );
 };
