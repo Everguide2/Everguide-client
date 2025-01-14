6bed7e0c39ce4a46a473fa3b0a40cb135149a81e
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-14 18:05:52 +0900

    [Chore] string 파일명 변경

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -2,5 +2,8 @@ export const strings = {
   findPolicy: "정책찾기",
   supportPolicy: "지원정책",
   eventEducation: "행사/교육",
-  workInformation: "일자리 정보"
+  workInformation: "일자리 정보",
+  LoginRegister: "로그인/회원가입",
+
+  searchBarPlaceHolder : "찾고 싶은 정책을 입력해보세요!",
 }
