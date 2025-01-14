ace002e781112f714fb1762450dded071d124609
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-14 17:59:39 +0900

    [Desing] Button 컴포넌트 제작

--- src/routes/pagePath.js
+++ src/routes/pagePath.js
@@ -1,4 +1,4 @@
-export const path = {
+export const pagePath = {
   FIND : 'find',
   SUPPORT : 'support',
   EVENT : 'event',
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -5,7 +5,6 @@ const routes =  [
     path: '/',
     element: <RootLayout />,
     children: [{
-
     }],
   },
 ]
