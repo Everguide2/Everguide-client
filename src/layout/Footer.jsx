4278e6f705cadb91f7dc22769b9eb82d8062f719
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-14 17:52:29 +0900

    [Chore] Footer theme 받아오 방법 수정

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -12,11 +12,11 @@ export default Footer;
 
 const Container = styled.div`
   width: 100%;
-  background-color: ${(props) => props.theme.colors.gray[50]};
+  background-color: ${({theme}) => theme.colors.gray[50]};
 `
 
 const FooterContainer = styled.div`
-  width: ${(props) => props.theme.size.safeArea};  
+  width: ${({theme}) => theme.size.safeArea};  
   height: 184px;
   margin: 0 auto;
 `
