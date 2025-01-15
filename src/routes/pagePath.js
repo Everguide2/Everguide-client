4859875d1fd3fa687c82880f9d7e49a8cb7da409
wnswo22 <zldwo215@gamil.com>
2025-01-15 21:37:49 +0900

    [feat] 정책추천 페이지 초기 설정

--- src/routes/pagePath.js
+++ src/routes/pagePath.js
@@ -1,6 +1,9 @@
+import { Policy } from "../pages";
+
 export const pagePath = {
   FIND : 'find',
   SUPPORT : 'support',
   EVENT : 'event',
   WORK : 'work',
+  POLICY : 'policy'
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,14 @@
 import RootLayout from "../layout/rootLayout";
 import {pagePath} from "./pagePath.js";
+import { Policy } from "../pages/PolicyRec/ui";
 
 const routes =  [
   {
     path: '/',
     element: <RootLayout />,
-    children: [{
+    children: [{ 
+      path : pagePath.POLICY,
+      element : <Policy/>
     }],
   },
 ]
