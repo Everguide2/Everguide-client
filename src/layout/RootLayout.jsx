5316403feb62aec94c9edd55a6d44fbbf8f08897
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-16 19:21:06 +0900

    [Chore] Footer, Header 위치 변경에 따른 경로 변경

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,6 +1,6 @@
 import {Outlet} from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header/Header.jsx';
+import Footer from './footer/Footer.jsx';
 
 const RootLayout = () => {
   return (
