4d2dfa2d0730bb5d2d6853e5a1fbb6622eb93901
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-17 17:56:54 +0900

    [Design] Custom 탭메뉴 제작

---
+++ src/pages/event/components/TabMenu.jsx
@@ -0,0 +1,33 @@
+import {styled} from "styled-components";
+import {TabButton} from "../../../components";
+import {icTabList, icTabCard} from "../../../assets";
+import {useState, useEffect} from "react";
+
+const TabMenu = () => {
+  const [activeTab, setActiveTab] = useState("left");
+
+  const onClickTab = (tab) => {
+    setActiveTab(tab);
+    sessionStorage.setItem("activeTab", tab);
+  }
+
+  useEffect(() => {
+    const savedTabs = sessionStorage.getItem("activeTab");
+    if(savedTabs){
+      setActiveTab(savedTabs);
+    }
+  }, []);
+
+  return (
+      <Container>
+        <TabButton image={icTabCard} active={activeTab === "left"} onClick={() => onClickTab("left")} />
+        <TabButton image={icTabList} active={activeTab === "right"} onClick={() => onClickTab("right")} />
+      </Container>
+  );
+};
+
+export default TabMenu;
+
+const Container = styled.div`
+  display: flex;
+`
