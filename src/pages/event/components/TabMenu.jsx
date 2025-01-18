ada956871d07331ae0334415550213b80aa88c84
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-19 01:29:43 +0900

    [Refactor] TabMenu ActiveTab props 분리

--- src/pages/event/components/TabMenu.jsx
+++ src/pages/event/components/TabMenu.jsx
@@ -1,23 +1,9 @@
 import {styled} from "styled-components";
 import {TabButton} from "../../../components";
 import {icTabList, icTabCard} from "../../../assets";
-import {useState, useEffect} from "react";
 
-const TabMenu = () => {
-  const [activeTab, setActiveTab] = useState("left");
-
-  const onClickTab = (tab) => {
-    setActiveTab(tab);
-    sessionStorage.setItem("activeTab", tab);
-  }
-
-  useEffect(() => {
-    const savedTabs = sessionStorage.getItem("activeTab");
-    if(savedTabs){
-      setActiveTab(savedTabs);
-    }
-  }, []);
-
+// eslint-disable-next-line react/prop-types
+const TabMenu = ({activeTab, onClickTab}) => {
   return (
       <Container>
         <TabButton image={icTabCard} active={activeTab === "left"} onClick={() => onClickTab("left")} />
