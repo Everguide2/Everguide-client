73fb25b212836927694d05b0c1429a6ac9429aa1
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-20 00:39:07 +0900

    [Chore] ic-left, ic-right 아이콘 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,22 +1,24 @@
 // icons
-export {default as icMainLogo} from './icons/ic-main-logo.svg';
-export {default as icSubLogo} from './icons/ic-sub-logo.svg';
-export {default as icSearch} from './icons/ic-search.svg';
-export {default as icNotify} from './icons/ic-notify.svg';
-export {default as icFooterLogo} from './icons/ic-footer-logo.svg';
-export {default as icInstagram} from './icons/ic-instagram.svg';
-export {default as icKakao} from './icons/ic-kakao.svg';
-export {default as icNaver} from './icons/ic-naver.svg';
-export {default as icEyes} from './icons/ic-eyes.svg';
-export {default as icDown} from './icons/ic-down.svg';
-export {default as icDropDownDown} from './icons/ic-dropdown-down.svg';
-export {default as icTabCard} from './icons/ic-tab-card.svg';
-export {default as icTabList} from './icons/ic-tab-list.svg';
-export {default as icBookmark} from './icons/ic-bookmark.svg';
+export {default as icMainLogo} from '@assets/icons/ic-main-logo.svg';
+export {default as icSubLogo} from '@assets/icons/ic-sub-logo.svg';
+export {default as icSearch} from '@assets/icons/ic-search.svg';
+export {default as icNotify} from '@assets/icons/ic-notify.svg';
+export {default as icFooterLogo} from '@assets/icons/ic-footer-logo.svg';
+export {default as icInstagram} from '@assets/icons/ic-instagram.svg';
+export {default as icKakao} from '@assets/icons/ic-kakao.svg';
+export {default as icNaver} from '@assets/icons/ic-naver.svg';
+export {default as icEyes} from '@assets/icons/ic-eyes.svg';
+export {default as icDown} from '@assets/icons/ic-down.svg';
+export {default as icDropDownDown} from '@assets/icons/ic-dropdown-down.svg';
+export {default as icTabCard} from '@assets/icons/ic-tab-card.svg';
+export {default as icTabList} from '@assets/icons/ic-tab-list.svg';
+export {default as icBookmark} from '@assets/icons/ic-bookmark.svg';
+export {default as icRight} from "@assets/icons/ic-right.svg";
+export {default as icLeft} from "@assets/icons/ic-left.svg";
 
 // images
 
 // fonts
-export {default as NanumGothicRegular } from './fonts/NanumGothic-Regular.ttf';
-export {default as NanumGothicBold } from './fonts/NanumGothic-Bold.ttf';
-export {default as NanumGothicExtraBold } from './fonts/NanumGothic-ExtraBold.ttf';
+export {default as NanumGothicRegular } from '@assets/fonts/NanumGothic-Regular.ttf';
+export {default as NanumGothicBold } from '@assets/fonts/NanumGothic-Bold.ttf';
+export {default as NanumGothicExtraBold } from '@assets/fonts/NanumGothic-ExtraBold.ttf';
