168be5ea2d8138f0ea9c05f8ae78bf5b7ce1df4b
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-01-20 20:17:05 +0900

    [feat] 각 블록들 홈 컴포넌트에 연결

--- src/pages/home/ui/Home.jsx
+++ src/pages/home/ui/Home.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import "./style.js";
+import theme from "../../../theme/theme.js";
+import DummyData from "../../../test/dummy.js";
+import PoliciesThisWeek from "../components/PoliciesThisWeek.jsx";
+import NewPolicies from "../components/NewPolicies.jsx";
+import Advertising from "../components/Advertising.jsx";
+import ParticipatingEv from "../components/ParticipatingEv.jsx";
+import WeeklyJobInfo from "../components/WeeklyJobInfo.jsx";
 const Home = () => {
+  const [dummy, setDummy] = useState(DummyData);
   return (
-    <div>
-      <p>홈화면</p>
-    </div>
+    <Container>
+      <PoliciesThisWeek data={dummy} />
+      <NewPolicies data={dummy} />
+      <Advertising />
+      <ParticipatingEv data={dummy.참여할만한행사프로그램} />
+      <WeeklyJobInfo data={dummy.이번주의일자리정보} />
+    </Container>
   );
 };
 
 export default Home;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  // background-color: black;
+`;
