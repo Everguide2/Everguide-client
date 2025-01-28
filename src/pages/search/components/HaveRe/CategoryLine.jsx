42315986c7617958da671a7181273a7e1940f023
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-01-28 18:43:14 +0900

    [Feat] 검색결과 있을 시_ 카테고리별 수 출력 컴포넌트 생성

---
+++ src/pages/search/components/HaveRe/CategoryLine.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import theme from "../../../../theme/theme";
+
+const CategoryLine = ({ category, categoryCount }) => {
+  return (
+    <Categories>
+      {category.map((name, index) => {
+        const isLast = index === category.length - 1;
+        return categoryCount[index] > 0 ? (
+          <>
+            <EachCategory isOn={true} key={index}>
+              {name}
+            </EachCategory>
+            <CountCategory>({categoryCount[index]})</CountCategory>
+            {!isLast && <Contour>|</Contour>}
+          </>
+        ) : (
+          <>
+            <EachCategory isOn={false} key={index}>
+              {name}
+            </EachCategory>
+            <CountCategory>({categoryCount[index]})</CountCategory>
+          </>
+        );
+      })}
+    </Categories>
+  );
+};
+
+export default CategoryLine;
+
+const Categories = styled.p`
+  color: black;
+  height: 100px;
+  margin-left: 110px;
+  margin-top: 20px;
+  display: flex;
+  ${({ theme }) => theme.fonts.subHeader5};
+`;
+const EachCategory = styled.div`
+  color: ${({ isOn, theme }) =>
+    isOn ? theme.colors.primary[500] : theme.colors.gray[500]};
+`;
+const CountCategory = styled.p`
+  color: ${({ theme }) => theme.colors.gray[500]};
+`;
+const Contour = styled.p`
+  margin-left: 12px;
+  margin-right: 12px;
+  color: ${({ theme }) => theme.colors.gray[500]};
+`;
