8b8f947874383ce4584f0033a627010d69eb835d
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-01-28 20:50:34 +0900

    [Design] Header 상단 고정 및 z-index 값 최상위로 수정

--- src/layout/Header/ui/style.js
+++ src/layout/Header/ui/style.js
@@ -2,7 +2,9 @@ import {styled} from "styled-components";
 
 export const Container = styled.header`
   position: sticky;
+  top: 0;
   width: 100%;
   background-color: ${(props) => props.theme.colors.white};
   box-shadow: 0 0 29px rgba(0,0,0,0.13);
+  z-index: 9999;
 `
