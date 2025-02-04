6b7dfd901026bfffe08d8ebe3fe2afd4e463dbfd
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-02-04 16:09:18 +0900

    [Refactor] store 네이밍 규칙에 맞게 alarm이름 변경

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,7 +3,7 @@ import informationReducer from "@stores/common/informationSlice";
 import pagingReducer from "@stores/paging/pagingSlice";
 import detailReducer from "@stores/common/detailSlice";
 import authReducer from '@stores/auth/authSlice';
-import alarmSlice from "@stores/alarm/alarmSlice";
+import alarmReducer from "@stores/alarm/alarmSlice";
 import SearchReducer from "@stores/search/SearchSlice.js";
 
 export default configureStore({
@@ -12,7 +12,7 @@ export default configureStore({
     paging: pagingReducer,
     detail: detailReducer,
     auth: authReducer,
-    alarm: alarmSlice,
+    alarm: alarmReducer,
     Search: SearchReducer,
   }
 });
