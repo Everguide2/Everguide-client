1232d84f25592aee1e898362f8c1d801702a0783
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-02-06 20:21:13 +0900

    [Fix] 배포 오류 수정 취소

--- src/layout/Sidebar/ui/SideBar.jsx
+++ src/layout/Sidebar/ui/SideBar.jsx
@@ -2,49 +2,9 @@ import {icEyes} from "../../../assets";
 import {string} from "../../../constants";
 import * as S from "./style.js"
 import {FilterTitle, FilterSection, EverguideManual} from "../components";
+import {sideBarData} from "../feature/sidebarData";
 
 const SideBar = () => {
-  const sideBarData = [
-    {
-      title: "분야",
-      items: [
-        { id: "job", label: "일자리", count: 50 },
-        { id: "housing", label: "주거", count: 50 },
-        { id: "education", label: "교육문화", count: 50 },
-        { id: "medical", label: "의료", count: 50 },
-        { id: "other", label: "기타", count: 50 },
-      ],
-    },
-    {
-      title: "마감일",
-      items: [
-        { id: "less_7", label: "7일 미만", count: 50 },
-        { id: "less_30", label: "30일 미만", count: 50 },
-        { id: "more_30", label: "30일 이상", count: 50 },
-        { id: "open", label: "상시 접수", count: 50 },
-        { id: "closed", label: "마감", count: 50 },
-      ],
-    },
-    {
-      title: "지역",
-      items: [
-        { id: "nationwide", label: "전국", count: 401 },
-        { id: "seoul", label: "서울", count: 50 },
-        { id: "incheon", label: "인천", count: 50 },
-        { id: "busan", label: "부산", count: 50 },
-        { id: "gyeonggi", label: "경기도", count: 50 },
-        { id: "chungcheongnam", label: "충청남도", count: 50 },
-        { id: "chungcheongbuk", label: "충청북도", count: 50 },
-        { id: "jeollanam", label: "전라남도", count: 50 },
-        { id: "jeollabuk", label: "전라북도", count: 50 },
-        { id: "gyeongsangbuk", label: "경상북도", count: 50 },
-        { id: "gyeongsangnam", label: "경상남도", count: 50 },
-        { id: "kwangwon", label: "강원도", count: 50 },
-        { id: "jeju", label: "제주도", count: 50 },
-      ],
-    },
-  ];
-
   return (
       <S.Container>
         <S.FilterContainer>
