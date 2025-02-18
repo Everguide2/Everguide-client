e5b4ddda06dda5263de0c8f8d5c326fb45d3926f
3tjwls7 <seojin53185318@gmail.com>
2025-02-18 17:52:34 +0900

    [Design] 마이페이지 ui수정정

--- src/pages/MyPage/components/modal/CommonModal.jsx
+++ src/pages/MyPage/components/modal/CommonModal.jsx
@@ -41,13 +41,12 @@ const Overlay = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  height: 100vh; 
   background: rgba(0, 0, 0, 0.5);
   display: flex;
   align-items: center;
   justify-content: center;
-  z-index: 1000;
-  padding-top: 100px;
+  z-index: 9999; 
 `;
 
 const ModalContainer = styled.div`
