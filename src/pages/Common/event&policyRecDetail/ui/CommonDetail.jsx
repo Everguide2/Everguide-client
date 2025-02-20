ea54fc00c66a61846bf7b85e99b24c2cf87a89d2
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-02-20 20:36:52 +0900

    [Fix] 로그인 유무에 따라 북마크 표시 처리

--- src/pages/Common/event&policyRecDetail/ui/CommonDetail.jsx
+++ src/pages/Common/event&policyRecDetail/ui/CommonDetail.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
+import {useValidateLogin} from "@hooks/useValidateLogin.js";
 import * as S from "./style.js";
 import {SmallButton, LargeButton, BookMark, EventContent, JobContent, PolicyContent} from "@pages/Common/event&policyRecDetail/components/";
 import ArticleSection from "@pages/Common/event&policyRecDetail/components/ArticleSection.jsx";
@@ -11,7 +12,7 @@ import {string} from "@constants/";
 // 정책 지원, 행사/교육, 일자리 정보 공통 사용
 const CommonDetail = ({type}) => {
   const navigate = useNavigate();
-  const [isLogin, setIsLogin] = useState(true);
+  const {isLogin} = useValidateLogin();
 
   const detail = useSelector(state => {
     if (type === "지원정책") return state.policyDetail;
